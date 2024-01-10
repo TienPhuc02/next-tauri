a934544cb39dae8a94256229eabdb8b2f46e5eee
TienPhuc02 <dtp2352002@gmail.com>
2024-01-10 10:54:28 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
-import Image from 'next/image'
+"use client";
+import axios from "axios";
+import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  const callTestApi = async () => {
+    const res = await axios.get("http://localhost:8002/api/v1/products");
+    if (res) {
+      console.log(res.data.products[1]);
+      localStorage.setItem("test", res.data.products[1].name);
+    }
+  };
+  useEffect(() => {
+    callTestApi();
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -15,7 +28,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -47,7 +60,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            Docs{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -64,7 +77,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
+            Learn{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -81,7 +94,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
+            Templates{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -98,7 +111,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
+            Deploy{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -109,5 +122,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
